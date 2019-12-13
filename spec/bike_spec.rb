d0bcb4b61555fc7266b68c493a7228d80edfa3e9
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2019-12-13 16:19:12 +0000

    Step 18 - Dealing with broken bikes

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,22 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to :working?}
+
+  # it { is_expected.to respond_to :report_broken }
+   #it { is_expected.to respond_to :broken? }
+
+  #it 'can be reported as broken' do
+    #subject.report_broken
+    #expect(subject).to be_broken
+  #end 
+
+  it 'upon initialization, the bike cant be reported as broken' do
+    expect(subject.broken).to eq false 
+  end 
+
+  it 'if the bike is reported as broken the broken status changes to true' do 
+    subject.report_broken
+    expect(subject.broken).to eq true
+  end 
+
 end  
